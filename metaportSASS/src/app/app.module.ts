42d1bacab071f0ff7ea32a2fc8f1f65f9ecde23d
fabriciominowa <fabriciominowa12@gmail.com>
2023-08-31 18:59:38 -0300

    port-sass

---
+++ metaportSASS/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Views/header/header.component';
+import { HomeComponent } from './Views/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
